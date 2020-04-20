b21ffe58a6dbb9f825164d4ca18de9af73b862c1
Sergio Colado <sergiocoladoloro@gmail.com>
2020-04-20 20:22:18 +0200

    Initial Commit

---
+++ MirrorChess/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MirrorChess
+//
+//  Created by Sergio Colado on 20.04.20.
+//  Copyright © 2020 Sergio Colado. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
